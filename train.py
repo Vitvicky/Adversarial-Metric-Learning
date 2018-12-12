3cff6d1eca8ed6f6470b0a0f5b3caae0db0fdc71
ZHUOYI WANG <sduwzy1992@gmail.com>
2018-12-11 23:27:09 -0600

    Update train.py

--- train.py
+++ train.py
@@ -1,79 +1,95 @@
 import torch
 import numpy as np
-from networks import EmbeddingNet, TripletNet
-from losses import TripletLoss
 from network import *
 from loss import *
+from network import *
+from loss import *
+import torch.optim as optim
+from torch.autograd.variable import Variable
+
+
+generate = GeneratorNet()
+generate.cuda()
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
 
 def get_generator_input_sampler():
-    return lambda m, n: torch.rand(m, n)  
+    return lambda m, n: torch.rand(m, n)
 
-def train_epoch(train_loader, model, cuda, margin, lambda1, lambda2):
-	# Model parameters
-    g_input_size = 1      # Random noise dimension coming into generator, per output vector
-    g_hidden_size = 50     # Generator complexity
-    g_output_size = 100     # Size of generated output vector
-    d_input_size = 784    # Minibatch size - cardinality of distributions
-    d_hidden_size = 300    # Discriminator complexity
-    d_output_size = 100     # Single dimension for 'real' vs. 'fake' classification
-	optim_betas = (0.9, 0.999)
-	
-	gi_sampler = get_generator_input_sampler()
-	
+
+def noise(size):
+    n = Variable(torch.randn(size, 784)).cuda()
+    return n
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    losses = AverageMeter()
+
+    # switch to train mode
     model.train()
-    losses = []
-    total_loss = 0
-	
-    G = Generator(input_size=g_input_size,
-                  hidden_size=g_hidden_size,
-                  output_size=g_output_size,
-                  f=generator_activation_function)
-    g_optimizer = optim.Adam(G.parameters(), lr=0.01, betas=optim_betas)
-    net_optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-	 
-    criterion_adv = generateLoss(margin, lambda1, lambda2)  
-    criterion_triplet = TripletLoss(margin)
-	
-	# start to train
     for batch_idx, (data1, data2, data3) in enumerate(train_loader):
-        if args.cuda:
-            data1, data2, data3 = data1.cuda(), data2.cuda(), data3.cuda()
-        data1, data2, data3 = Variable(data1), Variable(data2), Variable(data3)
-
-
-        net_optimizer.zero_grad()
-		g_optimizer.zero_grad()
-		
+        data1, data2, data3 = data1.cuda(), data2.cuda(), data3.cuda()
+        N = data1.size(0)
+        # print("N is: ", N)
+        # compute output
         embedded_x, embedded_y, embedded_z = model(data1, data2, data3)
-
-		# generate adversarial example
-		adv_example = ()
-		
-        loss_adv = criterion_adv(embedded_x, adv_example, embedded_y, embedded_z)
-		loss_trip = criterion_triplet(embedded_x, embedded_y, embedded_z)
-        loss = loss_adv + 0.001 * loss_trip
-        losses.append(loss.item())
-        total_loss += loss.item()
+        noise_data = noise(N)
+        # print("noise data is: ", noise_data)
+        fake_data = generate(noise_data)
+        # print(fake_data)
+        # loss_triplet = criterion(embedded_x, embedded_y, embedded_z)
+        loss = criterion(embedded_x, fake_data, embedded_y, embedded_z)
+        losses.update(loss.data[0], data1.size(0))
+        #
+        # # compute gradient and do optimizer step
+        optimizer.zero_grad()
         loss.backward()
-        net_optimizer.step()
+        optimizer.step()
 
-       
-        if batch_idx % log_interval == 0:
-            message = 'Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                batch_idx * len(data[0]), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), np.mean(losses))
-            for metric in metrics:
-                message += '\t{}: {}'.format(metric.name(), metric.value())
+        if batch_idx % 5 == 0:
+            print('Train Epoch: {} [{}/{}]\t'
+                  'Loss: {:.4f} ({:.4f})'.format(
+                epoch, batch_idx * len(data1), len(train_loader.dataset),
+                losses.val, losses.avg))
 
-            print(message)
-            losses = []
 
-    total_loss /= (batch_idx + 1)
-    return total_loss, metrics
+if __name__ == "__main__":
+    dataset_path = '/home/wzy/Coding/Data/metric_learning/mnist_normal.csv'
+    dataset, classes = read_dataset(dataset_path)
+    class_count = len(classes)
+    test_data = dataset[:100]
+    margin = 1
+    lambda1 = 0.1
+    lambda2 = 0.5
+    epochs = 5
 
-	
-margin = 1.
-embedding_net = EmbeddingNet()
-model = TripletNet(embedding_net)
-if cuda:
+    triplet_dataset = TripletMNIST(test_data, 200)
+    net = EmbeddingNet()
+    model = TripletNet(net)
     model.cuda()
+
+    # criterion_triplet = TripletLoss(margin)
+    criterion = generateLoss(margin, lambda1, lambda2)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    train_dataloader = DataLoader(dataset=triplet_dataset, shuffle=True, batch_size=10)
+
+    for epoch in range(1, epochs + 1):
+        # train for one epoch
+        train(train_dataloader, model, criterion, optimizer, epoch)
