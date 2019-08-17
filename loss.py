54fbc1bb297209ee6b5b6d6192531431ad6a8d29
ZHUOYI WANG <sduwzy1992@gmail.com>
2019-08-17 17:14:47 -0500

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,6 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
+
+
+class NormalDistance(torch.nn.Module):
+    def __init__(self):
+        super(NormalDistance, self).__init__()
+
+    def forward(self, x1, x2, eps=1e-6):
+        assert x1.size() == x2.size()
+        diff = torch.abs(x1 - x2)
+        out = torch.pow(diff, 2).sum(dim=1, keepdim=True)
+
+        return out
 
 
 class PairwiseDistance(torch.nn.Module):
@@ -16,23 +29,21 @@ class PairwiseDistance(torch.nn.Module):
         return torch.pow(out, 1. / self.norm)
 
 
-class NormalDistance(torch.nn.Module):
-    def __init__(self):
-        super(NormalDistance, self).__init__()
+class CosineSimilarity(torch.nn.Module):
+    def __init__(self, dim=1, eps=1e-8):
+        super(CosineSimilarity, self).__init__()
+        self.dim = dim
+        self.eps = eps
 
-    def forward(self, x1, x2, eps=1e-6):
-        assert x1.size() == x2.size()
-        diff = torch.abs(x1 - x2)
-        out = torch.pow(diff + eps, 2).sum(dim=1, keepdim=True)
-
-        return out
+    def forward(self, x1, x2):
+        return F.cosine_similarity(x1, x2, self.dim, self.eps)
 
 
 class TripletLoss(nn.Module):
     def __init__(self, margin):
         super(TripletLoss, self).__init__()
         self.margin = margin
-        self.pdist = NormalDistance()
+        self.pdist = PairwiseDistance()
 
     def forward(self, anchor, positive, generate, size_average=True):
         d_p = self.pdist.forward(anchor, positive)
@@ -43,106 +54,94 @@ class TripletLoss(nn.Module):
         loss = torch.mean(dist_hinge)
         return loss
 
-        # distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
-        # distance_negative = (anchor - generate).pow(2).sum(1)  # .pow(.5)
-        # losses = F.relu(distance_positive - distance_negative + self.margin)
-        # return losses.mean() if size_average else losses.sum()
 
-
-class OnlineTripletLoss(nn.Module):
-    def __init__(self, margin, triplet_selector):
-        super(OnlineTripletLoss, self).__init__()
+class ContrastiveLoss(nn.Module):
+    def __init__(self, margin):
+        super(ContrastiveLoss, self).__init__()
         self.margin = margin
-        self.triplet_selector = triplet_selector
+        self.eps = 1e-9
 
-    def forward(self, embeddings, target):
-        triplets = self.triplet_selector.get_triplets(embeddings, target)
-
-        if embeddings.is_cuda:
-            triplets = triplets.cuda()
-
-        ap_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 1]]).pow(2).sum(1)  # .pow(.5)
-        an_distances = (embeddings[triplets[:, 0]] - embeddings[triplets[:, 2]]).pow(2).sum(1)  # .pow(.5)
-        losses = F.relu(ap_distances - an_distances + self.margin)
-
-        return losses.mean(), len(triplets)
+    def forward(self, output1, output2, target, size_average=True):
+        distances = (output2 - output1).pow(2).sum(1)  # squared distances
+        losses = 0.5 * (target.float() * distances +
+                        (1 + -1 * target).float() * F.relu(self.margin - (distances + self.eps).sqrt()).pow(2))
+        return losses.mean() if size_average else losses.sum()
 
 
-class generateLoss(nn.Module):
-    def __init__(self, margin, lambda1, lambda2):
-        super(generateLoss, self).__init__()
+class BinomialLoss(nn.Module):
+    def __init__(self, alpha=50, margin=0.5, beta=0.5, penalty=1):
+        super(BinomialLoss, self).__init__()
+        self.pdist = nn.CosineSimilarity(dim=1, eps=1e-6)
+        self.penalty = penalty
+        self.alpha = alpha
+        self.beta = beta
         self.margin = margin
-        self.lambda1 = lambda1
-        self.lambda2 = lambda2
-        self.pdist = PairwiseDistance(2)
-        self.dist = NormalDistance()
 
-    def forward(self, anchor_ori, anchor_d, positive_ori, pos_d, negative_ori, neg_d, generate_ori, gen_d, class_statis, size_average=True):
-        generate_ori = generate_ori.view(generate_ori.size(0), 3072)
-        anchor_ori = anchor_ori.view(anchor_ori.size(0), 3072)
-        negative_ori = negative_ori.view(negative_ori.size(0), 3072)
-        distance_1 = self.dist(generate_ori, anchor_ori)  # .pow(.5)
-        # distance_2 = self.dist(generate_ori, negative_ori)  # .pow(.5)
+    def forward(self, output1, output2, target, size_average=True):
+        # output1_T = output1.t()
+        # distances = output1_T * output2 / (torch.norm(output1, p=1) * torch.norm(output2, p=1))  # squared distances
+        losses = 0
+        c = 0
+        for i in range(0, len(target)):
+            c += 1
+            output1_item = output1[i]
+            output2_item = output2[i]
+            output1_vec = output1_item.unsqueeze(0)
+            output2_vec = output2_item.unsqueeze(0)
 
-        # confusion
-        # anchor = anchor_set[0]
-        # negative = negative_set[0]
-        # anchor = anchor.cuda()
-        # negative = negative.cuda()
-        # anchor_label = anchor_set[1]
-        # negative_label = negative_set[1]
-        # distance_2 = 0
-        # for i in range(0, len(anchor)):
-        #     anchor_item = anchor[i]
-        #     negative_item = negative[i]
-        #     joint_prob_a = 1 / class_statis[anchor_label[i].item()]
-        #     joint_prob_n = 1 / class_statis[negative_label[i].item()]
-        #     distance_2 += joint_prob_a * joint_prob_n * self.pdist(anchor_item, negative_item)
+            # distances_item = (output1_vec - output2_vec).pow(2).sum(1)
+            distances_item = self.pdist(output1_vec, output2_vec)
+            # print("distances_item: ", distances_item)
+            # print("target: ", target[i].item())
+            pos_loss = 0
+            neg_loss = 0
+            num_pos = 0
+            num_neg = 0
+            if target[i].item() == 1:
+                # self.penalty = 1
+                # print(torch.log(1 + torch.exp(-2 * (distances_item - self.margin))))
+                # pos_loss = math.log(1 + math.exp(-2 * (distances_item - self.margin)))
+                pos_loss += 2.0/self.beta * math.log(1 + math.exp(-self.beta*(distances_item - 0.5)))
+                num_pos += 1
 
-        distance_2 = 0.01 * self.dist.forward(anchor_ori, negative_ori)  # .pow(.5)
-        tmp_d1 = torch.log(distance_1 + 0.5)
-        tmp_d2 = torch.log(distance_2 + 0.5) + 1
+            else:
+                # self.penalty = 25
+                # neg_loss = 0.1 * math.log(1 + math.exp(self.alpha * (distances_item - self.margin)))
+                neg_loss += 2.0/self.alpha * math.log(1 + math.exp(self.alpha*(distances_item - 2.0)))
+                num_neg += 1
 
-        d_p = self.dist.forward(pos_d, anchor_d)
-        d_n = self.dist.forward(gen_d, anchor_d)
-        tmp1 = torch.log(d_p + 0.5)
-        tmp2 = torch.log(d_n + 0.5) + 1
-        dist_hinge = torch.clamp(tmp2 - self.margin - tmp1, min=0.0)
-        distance_adv = dist_hinge
+            # losses += pos_loss + neg_loss
+            # print("losses: ", losses)
+            # print("=====================")
+        losses = pos_loss/num_pos + neg_loss/num_neg
+        return losses
 
-        # gen_loss = distance_1 + self.lambda1 * distance_2 + self.lambda2 * F.relu(distance_adv)
-        gen_loss = tmp_d1 + self.lambda1 * tmp_d2 + self.lambda2 * distance_adv
-        return gen_loss.mean()
 
-# class generateLoss(nn.Module):
-#     def __init__(self, margin, lambda1, lambda2):
-#         super(generateLoss, self).__init__()
-#         self.margin = margin
-#         self.lambda1 = lambda1
-#         self.lambda2 = lambda2
+# class EC_Pair_Loss(nn.Module):
+#     def __init__(self):
+#         super(EC_Pair_Loss, self).__init__()
 #         self.pdist = PairwiseDistance(2)
 #
-#     def forward(self, anchor, generate, positive, negative, size_average=True):
-#         # pdist = nn.PairwiseDistance(p=2)
-#         distance_1 = (generate - anchor).pow(2).sum(1)  # .pow(.5)
-#         distance_2 = (generate - negative).pow(2).sum(1)  # .pow(.5)
+#     def forward(self, input1, input2, target, pos_num, neg_num, size_average=True):
+#         loss = 0
+#         w = 0
+#         for i in range(0, len(target)):
+#             anchor_item = input1[i].view(1, -1)
+#             comp_item = input2[i].view(1, -1)
+#             # print("size: ", anchor_item.shape)
+#             if target[i].item() == 1:
+#                 w = 1
+#             else:
+#                 w = 1/neg_num
 #
-#         # distance_adv = F.pairwise_distance(generate, anchor, 2)-F.pairwise_distance(positive, anchor, 2)-self.margin
-#         d_p = self.pdist.forward(generate, anchor)
-#         d_n = self.pdist.forward(positive, anchor)
-#         tmp1 = torch.log(d_p + 1)
-#         tmp2 = torch.log(d_n + 1)
-#         dist_hinge = torch.clamp(tmp2 - self.margin - tmp1, min=0.0)
-#         distance_adv = dist_hinge
+#             loss += w * self.pdist(anchor_item, comp_item)
 #
-#         # gen_loss = distance_1 + self.lambda1 * distance_2 + self.lambda2 * F.relu(distance_adv)
-#         gen_loss = distance_1 + self.lambda1 * distance_2 + self.lambda2 * distance_adv
-#         return gen_loss.mean()
+#         loss_ec = math.log(1 + loss)
+#         return loss_ec
 
-
-class ECLoss(nn.Module):
+class EC_Pair_Loss(nn.Module):
     def __init__(self):
-        super(ECLoss, self).__init__()
+        super(EC_Pair_Loss, self).__init__()
         # self.margin = margin
         self.pdist = NormalDistance()
 
@@ -158,10 +157,12 @@ class ECLoss(nn.Module):
         # joint_prob_n = 1/class_statis[negative_label]
         # loss = joint_prob_a * joint_prob_n * (self.pdist(anchor, negative))
         for i in range(0, len(anchor)):
-            anchor_item = anchor[i]
-            negative_item = negative[i]
-            joint_prob_a = 1/class_statis[anchor_label[i].item()]
-            joint_prob_n = 1 / class_statis[negative_label[i].item()]
-            loss += joint_prob_a * joint_prob_n * self.pdist(anchor_item, negative_item)
+            anchor_item = anchor[i].view(1, -1)
+            negative_item = negative[i].view(1, -1)
+            if anchor_label[i].item() != negative_label[i].item():
+                joint_prob_a = 1/class_statis[anchor_label[i].item()]
+                joint_prob_n = 1 / class_statis[negative_label[i].item()]
+                loss += joint_prob_a * joint_prob_n * self.pdist(anchor_item, negative_item)
 
-        return loss.mean()
+        # math.log(1 + loss)
+        return math.log(1+loss.mean())
